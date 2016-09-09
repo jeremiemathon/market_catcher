cde3ff16a4acf45f662db87712d5a84228704a19
Jeremie Mathon <jeremie.mathon@gmail.com>
2016-09-09 14:52:21 +0200

    webserver starting point

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import sys
 import os
 from market_functions import *
 from print_functions import *
+from market_webserver import server
 
 
 if __name__ == '__main__':
 	d = fulfill_data_file()
+	#server.create_server_instance('0.0.0.0',8888,d)
 	while 1:
 		os.system('clear')
 		print_totals(d)
--- market_webserver/server.py
+++ market_webserver/server.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python3
- 
-import BaseHTTPServer
-import CGIHTTPServer
 
-def server_start(port, address, directory): 
-	server_address = (address, port)
+import http.server
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import ssl
+from .build_page import build_page
 
-	server = BaseHTTPServer.HTTPServer
-	handler = CGIHTTPServer.CGIHTTPRequestHandler
-	handler.cgi_directories = [directory]
-	print "Serveur actif sur le port :", PORT
+data=""
 
-	httpd = server(server_address, handler)
+class RequestHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		global data
+		# Send response status code
+		self.send_response(200)
+
+		# Send headers
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+
+		# Send message back to client
+		message = build_page(data)
+		# Write content as utf-8 data
+		self.wfile.write(bytes(message, "utf8"))
+		return
+
+
+def create_server_instance(server,port,d):
+	global data
+	data = d
+	httpd = http.server.HTTPServer((server, port), RequestHandler)
+	#httpd.socket = ssl.wrap_socket(httpd.socket, certfile='server.pem', server_side=True)
 	httpd.serve_forever()
